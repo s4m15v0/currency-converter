1a240b655761d155b5360b78cf016a2d45d49cc3
s4m15v0 <shamiulislam15@gmail.com>
2022-01-07 21:45:22 +0600

    function data added

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ fetch("https://api.frankfurter.app/currencies")
         display(data);
     });
 
+function display(data) {
+    const entries = Object.entries(data);
+    for (var i = 0; i < entries.length; i++) {
+        select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+        select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+    }
+}
+
